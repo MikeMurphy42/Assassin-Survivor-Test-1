c590f6630d3fc71eac7398de84bdb7daab5d3cb3
Mike Murphy <Mikemurphy2192015@gmail.com>
2023-05-28 19:38:58 -0500

    About to use ChatGPT to modify the levelUpSelectionButton Script.

--- Assets/Scripts/LevelUpSelectionButton.cs
+++ Assets/Scripts/LevelUpSelectionButton.cs
@@ -10,11 +10,27 @@ public class LevelUpSelectionButton : MonoBehaviour
     public TMP_Text upgradeDescText, nameLevelText;
     public Image weaponIcon;
 
+    private Weapon assignWeapon;
+
     public void UpdateButtonDisplay(Weapon theWeapon)
     {
         upgradeDescText.text = theWeapon.stats[theWeapon.weaponLvl].upgradeText;
         weaponIcon.sprite = theWeapon.icon;
 
         nameLevelText.text = theWeapon.name + " - LVL " + theWeapon.weaponLvl;
+
+        assignWeapon = theWeapon;
+    }
+
+
+    public void SelectUpgrade()
+    {
+        if (assignWeapon != null)
+        {
+            assignWeapon.LevelUp();
+            
+            UIController.instance.levelUpPanel.SetActive(false);
+            Time.timeScale = .2f;
+        }
     }
 }
